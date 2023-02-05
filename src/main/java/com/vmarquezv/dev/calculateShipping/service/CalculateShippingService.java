b3e0474f525009776b3e94b015e19550242b62ff
Marquezv <vmarquezv@outlook.com.br>
2023-02-05 15:02:51 -0300

    feat: Service - Added CEP verifications and calculate shipping system

--- src/main/java/com/vmarquezv/dev/calculateShipping/service/CalculateShippingService.java
+++ src/main/java/com/vmarquezv/dev/calculateShipping/service/CalculateShippingService.java
@@ -1,8 +1,10 @@
 package com.vmarquezv.dev.calculateShipping.service;
 
+import java.math.BigDecimal;
 import java.text.MessageFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,35 +15,112 @@ import com.vmarquezv.dev.calculateShipping.model.ViaCepResponse;
 @Service
 public class CalculateShippingService {
 	
-	@Autowired
-	private RestTemplate restTemplate = new RestTemplate();
+	private final RestTemplate restTemplate = new RestTemplate();
 	
 	@Autowired
 	private CheckCep checkCpf;
 	
 	public ShippingResponse requestCep(String cep) {
 		
-		if(checkCpf.isValid(cep)) {
-			String url = MessageFormat.format(
-					"https://viacep.com.br/ws/{0}/json/", 
-					cep);
+		cep = cep.replaceAll("[^a-zA-Z0-9]", "");
+		if(!checkCpf.isValid(cep)) {
 			
-			ViaCepResponse response = restTemplate.getForObject(url, ViaCepResponse.class);
-			
-			return toShippingResponse(response);
+			throw new ObjectNotFoundException("CEP - INVALID_CEP");
 		}
 		
-		throw new ObjectNotFoundException("CEP - NOT_FOUND");
+		String url = MessageFormat.format(
+				"https://viacep.com.br/ws/{0}/json/", 
+				cep);
+		
+		ResponseEntity<Object> responseEntity = restTemplate.getForEntity(url, Object.class);
+		
+		String obj = responseEntity.getBody().toString();
+		
+		ViaCepResponse response = restTemplate.getForObject(url, ViaCepResponse.class);
+		
+		if(obj.equals("{erro=true}")) throw new ObjectNotFoundException("CEP - NOT_FOUND");
+		
+		return toShippingResponse(response);
 	}
 	
 	
 	public ShippingResponse toShippingResponse(ViaCepResponse viaCep) {
-		
 		ShippingResponse response = new ShippingResponse(viaCep);
-		
+		String regiao = findRegiao(viaCep);
+		response.setRegiao(regiao);
+				
+		if(regiao != null) response.setFrete(calculateFrete(response.getRegiao()));
 		
 		return response;
 	}
 	
 	
+	
+	private String findRegiao(ViaCepResponse viaCep) {
+		String regiao = null;
+		String cep = viaCep.getCep();
+		
+		if(cep == null) return null;
+			
+		Integer n = Integer.parseInt(cep.substring(0, 5));
+		
+		if(n >= 01000 & n <= 39999) {
+			regiao = "Sudeste";
+		}
+		else if(n >= 70000 & n <= 76799 || n >= 78000 && n <= 79999) {
+			regiao = "CentroOeste";
+		}
+		else if(n >= 66000 & n <= 29) {
+			regiao = "Nordeste";
+		}
+		else if(n >= 80000 & n <= 99999) {
+			regiao = "Sul";
+		}
+		else if(n >= 66000 & n <= 69999 || n >= 76800 && n <= 77999) {
+			regiao = "Norte";
+		}
+		
+		return regiao;
+	}
+	
+	
+	private BigDecimal calculateFrete(String regiao) {
+		BigDecimal frete = new BigDecimal("0.00");
+		
+		switch (regiao) {
+		case "Sudeste":
+			frete = new BigDecimal("7.85");
+			break;
+		case "CentroOeste":
+			frete = new BigDecimal("12.50");
+			break;
+		case "Nordeste":
+			frete = new BigDecimal("15.98");
+			break;
+		case "Sul":
+			frete = new BigDecimal("17.30");
+			break;
+		case "Norte":
+			frete = new BigDecimal("20.83");
+			break;
+		default:
+			break;
+		}
+		
+		return frete;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+

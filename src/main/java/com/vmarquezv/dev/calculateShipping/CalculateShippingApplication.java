ad3271218a21418aeaf287ad46b3f00847f290f3
Marquezv <vmarquezv@outlook.com.br>
2023-02-06 19:40:16 -0300

    fix: Project - Added Swagger

--- src/main/java/com/vmarquezv/dev/calculateShipping/CalculateShippingApplication.java
+++ src/main/java/com/vmarquezv/dev/calculateShipping/CalculateShippingApplication.java
@@ -6,13 +6,15 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CalculateShippingApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CalculateShippingApplication.class, args);
 	}
-	
 	
 	@Bean 
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
--- src/main/java/com/vmarquezv/dev/calculateShipping/controller/CalculateShippingController.java
+++ src/main/java/com/vmarquezv/dev/calculateShipping/controller/CalculateShippingController.java
@@ -1,18 +1,22 @@
 package com.vmarquezv.dev.calculateShipping.controller;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.vmarquezv.dev.calculateShipping.exception.InvalidRequestException;
-import com.vmarquezv.dev.calculateShipping.exception.ObjectNotFoundException;
+import com.vmarquezv.dev.calculateShipping.exception.BadRequestException;
+import com.vmarquezv.dev.calculateShipping.model.CepRequest;
 import com.vmarquezv.dev.calculateShipping.model.ShippingResponse;
 import com.vmarquezv.dev.calculateShipping.service.CalculateShippingService;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+
 
 @RestController
 @RequestMapping("/v1/consulta-endereco")
@@ -21,19 +25,18 @@ public class CalculateShippingController {
 	@Autowired
 	private CalculateShippingService service;
 	
-	@PostMapping(consumes = "application/json", produces = "application/json")
-	public ResponseEntity<ShippingResponse> getCep(@RequestBody Map<String, String> cepRequest) {
-		String cep;
+	@RequestMapping(method = RequestMethod.POST)
+	@ApiResponses(value = {
+			@ApiResponse(code = 404, message = "CEP - NOT_FOUND" ),
+			@ApiResponse(code = 400, message = "CEP - INCORRECT_REQUEST")
+	})
+	@ApiOperation(value = "CalculateShipping - calculate frete with CEP")
+	public ResponseEntity<ShippingResponse> getCep(@RequestBody CepRequest cepRequest) {
+		String cep = cepRequest.getCep();
 		
-		try {
-			cep = cepRequest.get("cep").toString();
-		} catch (Exception e) {
-			throw new InvalidRequestException("INVALID REQUEST");
-		}
+		if(cep == null || cep.isEmpty()) throw new BadRequestException();
 		
-		if(cep == "" || cep.equals(null)) throw new ObjectNotFoundException("CEP - IS_NULL");
-		
-		return ResponseEntity.ok(service.requestCep(cep));
+		return ResponseEntity.ok(service.requestCep(cepRequest));
 	}
 	
 	

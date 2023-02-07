62e5ce65705c7a9583c9c86405449380fd865260
Marquezv <vmarquezv@outlook.com.br>
2023-02-07 18:59:25 -0300

    test: Test - Add checkCepTest

--- src/test/java/com/vmarquezv/dev/calculateShipping/service/CheckCepTest.java
+++ src/test/java/com/vmarquezv/dev/calculateShipping/service/CheckCepTest.java
@@ -1,36 +1,39 @@
 package com.vmarquezv.dev.calculateShipping.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.web.client.RestTemplate;
 
 @ExtendWith(MockitoExtension.class)
 public class CheckCepTest {
 	
-	@Mock
-	private RestTemplate restTemplate;
-	
-	@Mock
-	private CheckCep checkCep;
-	
 	@Spy
 	@InjectMocks
-	private CalculateShippingService service;
+	private CheckCep service;
+	
+	private final String CEP = "01001-000";
+
 	
 	@BeforeEach
 	void setUp() {
 		MockitoAnnotations.openMocks(this);
-		startUser();
 	}
 	
-	
-	private void startUser() {
+	@Test
+	public void whenIsValidCalled() {
+		when(service.isValid(CEP)).thenReturn(true);
+		
+		boolean isValid = service.isValid(CEP);
+		assertEquals(isValid, true);
 	}
+	
 	
 }
 
